a97b2b261514355a8b37207132e7f865685ef996
rzid <rdhanukpb@gmail.com>
2021-02-01 14:20:19 +0700

    Push final Readme and docs

--- docs/db_pos.sql
+++ docs/db_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 10, 2021 at 09:05 AM
+-- Generation Time: Feb 01, 2021 at 08:18 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -39,8 +39,13 @@ CREATE TABLE `tb_category` (
 --
 
 INSERT INTO `tb_category` (`id_category`, `name_category`) VALUES
-(1, 'Drinks'),
-(2, 'Foods');
+(1, 'Espresso Based'),
+(2, 'White'),
+(3, 'Tea'),
+(4, 'Blended'),
+(5, 'Tutti Fruit'),
+(6, 'Bakery'),
+(7, 'Lunch');
 
 -- --------------------------------------------------------
 
@@ -49,7 +54,7 @@ INSERT INTO `tb_category` (`id_category`, `name_category`) VALUES
 --
 
 CREATE TABLE `tb_history` (
-  `id_history` int(11) NOT NULL,
+  `id_history` int(10) NOT NULL,
   `date_history` datetime NOT NULL DEFAULT current_timestamp(),
   `cashier_history` varchar(255) NOT NULL,
   `product_history` text NOT NULL,
@@ -61,8 +66,30 @@ CREATE TABLE `tb_history` (
 --
 
 INSERT INTO `tb_history` (`id_history`, `date_history`, `cashier_history`, `product_history`, `amount_history`) VALUES
-(1, '2021-01-10 14:48:35', 'Cashier 1', 'Espresso x1, Coffee Latte x3, Cappucino x2', 65000),
-(2, '2021-01-10 14:51:33', 'Pevita Pearce', 'Espresso x5, Cappucino x4', 70000);
+(1, '2020-01-10 14:48:35', 'Cashier 1', 'Espresso x1, Coffee Latte x3, Cappucino x2', 65000),
+(2, '2021-01-16 17:36:52', 'Pevita Pearce', 'Espresso x1', 10000),
+(3, '2021-01-16 17:41:39', 'Pevita Pearce', 'Coffee Latte x7, Black Forest x1, Odading Mang Oleh x8', 175000),
+(4, '2021-01-16 17:42:47', 'Pevita Pearce', 'Choco Rhum x7', 196000),
+(5, '2021-01-16 17:45:12', 'Pevita Pearce', 'Coffee Latte x7, Red Velvet Latte x4, Choco Rhum x3', 321000),
+(6, '2021-01-16 17:46:18', 'Pevita Pearce', 'Espresso x1, Cappucino x1', 15000),
+(7, '2021-01-16 17:47:43', 'Pevita Pearce', 'Cappucino x1', 5000),
+(8, '2021-01-16 10:59:08', 'Pevita Pearce', 'Coffee Latte x1', 15000),
+(9, '2021-01-17 08:14:27', 'Pevita Pearce', 'Choco Rhum x4, Black Forest x1, Green Tea x3', 169000),
+(10, '2021-01-17 12:01:23', 'Pevita Pearce', 'Coffee Latte x1, Cappucino x1', 20000),
+(11, '2021-01-17 12:01:28', 'Pevita Pearce', 'Cappucino x1, Black Forest x1', 35000),
+(12, '2021-01-17 12:01:32', 'Pevita Pearce', 'Choco Rhum x1', 28000),
+(13, '2021-01-17 12:01:37', 'Pevita Pearce', 'Red Velvet Latte x5, Choco Rhum x1', 193000),
+(14, '2021-01-17 12:01:47', 'Pevita Pearce', 'Chicken Katsu Dabu-dabu x1, Salmon Truffle Teriyaki x1, Wiener Schnitzel x1, Green Tea x1, Dalgona Coffee x1', 233000),
+(15, '2021-01-17 12:01:56', 'Pevita Pearce', 'Espresso x1, Coffee Latte x1, Cappucino x1, Choco Rhum x1, Black Forest x1', 88000),
+(16, '2021-01-17 12:02:00', 'Pevita Pearce', 'Red Velvet Latte x1, Choco Rhum x1, Black Forest x1', 91000),
+(17, '2021-01-17 16:53:01', 'Pevita Pearce', 'Espresso x1', 10000),
+(18, '2021-01-19 09:33:36', 'Pevita Pearce', 'Choco Rhum x6', 168000),
+(19, '2021-01-20 09:24:06', 'Pevita Pearce', 'Coffee Latte x1, Cappucino x5, Choco Rhum x1, Black Forest x1, Green Tea x3', 137500),
+(20, '2021-01-20 09:43:58', 'Pevita Pearce', 'Coffee Latte x1', 16500),
+(21, '2021-01-20 10:34:10', 'Pevita Pearce', 'Coffee Latte x1, Cappucino x2, Choco Rhum x1', 58300),
+(22, '2021-01-21 08:14:13', 'Pevita Pearce', 'Cappucino x1, Black Forest x1, Salmon Truffle Teriyaki x1, Green Tea x1', 114400),
+(23, '2021-01-21 14:02:59', 'Pevita Pearce', 'Coffee Latte x1, Cappucino x1, Choco Rhum x1, Black Forest x1', 85800),
+(24, '2021-01-21 14:04:35', 'Pevita Pearce', 'Dalgona Coffee x1, Macchiato x1', 71500);
 
 -- --------------------------------------------------------
 
@@ -74,8 +101,8 @@ CREATE TABLE `tb_product` (
   `id_product` int(11) NOT NULL,
   `name_product` varchar(255) NOT NULL,
   `price_product` int(100) NOT NULL,
-  `category_product` int(10) NOT NULL,
-  `image_product` varchar(100) NOT NULL
+  `category_product` int(11) NOT NULL,
+  `image_product` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -83,15 +110,75 @@ CREATE TABLE `tb_product` (
 --
 
 INSERT INTO `tb_product` (`id_product`, `name_product`, `price_product`, `category_product`, `image_product`) VALUES
-(1, 'Espresso', 10000, 1, 'espresso.jpg'),
-(2, 'Coffee Latte', 15000, 1, 'latte.jpg'),
-(3, 'Cappucino', 5000, 1, 'cappucino.jpg'),
-(4, 'Red Velvet Latte', 33000, 1, 'velvet.jpg'),
-(5, 'Choco Rhum', 28000, 2, 'choco_rhum.jpg'),
-(6, 'Black Forest', 30000, 2, 'black_forest.jpg'),
-(7, 'Chicken Katsu Dabu-dabu', 60000, 2, 'katsu.jpg'),
-(8, 'Salmon Truffle Teriyaki', 60000, 2, 'truffle_teriyaki.jpg'),
-(9, 'Wiener Schnitzel', 69000, 2, 'wiener.jpg');
+(1, 'Espresso', 10000, 1, '1611739111577.png'),
+(2, 'Coffee Latte', 15000, 1, '1611739234649.png'),
+(3, 'Cappucino', 5000, 1, '1611739273846.png'),
+(4, 'Red Velvet Latte', 33000, 2, '1611739306022.png'),
+(5, 'Choco Rhum', 28000, 6, '1611739341236.png'),
+(6, 'Black Forest', 30000, 6, '1611739413266.png'),
+(7, 'Chicken Katsu Dabu-dabu', 60000, 7, '1611739463283.png'),
+(8, 'Salmon Truffle Teriyaki', 60000, 7, '1611739493568.png'),
+(9, 'Wiener Schnitzel', 69000, 7, '1611739517892.png'),
+(10, 'Affogato', 25000, 1, '1611741704975.jpeg'),
+(11, 'Americano', 20000, 1, '1611741729543.jpg'),
+(12, 'Macchiato', 20000, 1, '1611741754450.png'),
+(13, 'Mochachino', 25000, 1, '1611741779906.jpg'),
+(14, 'Green Tea Latte', 25000, 2, '1611741992657.jpg'),
+(15, 'Taro Latte', 25000, 2, '1611742008218.jpg'),
+(16, 'Hot Chocolate', 25000, 2, '1611742056811.jpg'),
+(17, 'Hot Choco Mint', 25000, 2, '1611742071930.jpg'),
+(18, 'Lychee Tea', 19000, 3, '1611742309502.jpg'),
+(19, 'Strawberry Tea', 19000, 3, '1611742328502.jpg'),
+(20, 'Peach Tea', 19000, 3, '1611742347876.jpg'),
+(21, 'Chocolate Delight', 25000, 4, '1611742558482.jpg'),
+(22, 'Green Tea Frap', 25000, 4, '1611742570830.png'),
+(23, 'Caramel Frap', 25000, 4, '1611742586145.jpg'),
+(24, 'Blended Choco Mint', 25000, 4, '1611742602733.jpg'),
+(25, 'Sourberry', 19000, 5, '1611742723014.jpg'),
+(26, 'Mucho Mango', 19000, 5, '1611742732823.jpg'),
+(27, 'Summer Blast', 30000, 5, '1611793306496.jpg'),
+(31, 'Avocado Lava', 40000, 5, '1611925909999.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_role`
+--
+
+CREATE TABLE `tb_role` (
+  `id_role` int(11) NOT NULL,
+  `name_role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_role`
+--
+
+INSERT INTO `tb_role` (`id_role`, `name_role`) VALUES
+(1, 'Admin'),
+(2, 'Cashier');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `name_user` varchar(255) NOT NULL,
+  `email_user` varchar(255) NOT NULL,
+  `password_user` varchar(255) NOT NULL,
+  `access_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `name_user`, `email_user`, `password_user`, `access_user`) VALUES
+(1, 'admin', 'admin@kopimu.com', '$2b$10$9jx1yVkyLz15znqoj.ZbkOH4CN.puBHgX.hakDsNPA3fqPOM5T.iC', 1),
+(2, 'cashier 1', 'cashier@kopimu.com', '$2b$10$EDRkJfz28rgdGDGJvJlRReVx1kRyPEcPX9tpkglgoHQD0HSAwF1ma', 2);
 
 --
 -- Indexes for dumped tables
@@ -101,7 +188,8 @@ INSERT INTO `tb_product` (`id_product`, `name_product`, `price_product`, `catego
 -- Indexes for table `tb_category`
 --
 ALTER TABLE `tb_category`
-  ADD PRIMARY KEY (`id_category`) USING BTREE;
+  ADD PRIMARY KEY (`id_category`) USING BTREE,
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- Indexes for table `tb_history`
@@ -117,6 +205,19 @@ ALTER TABLE `tb_product`
   ADD KEY `category_product` (`category_product`);
 
 --
+-- Indexes for table `tb_role`
+--
+ALTER TABLE `tb_role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `access_user` (`access_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -124,19 +225,31 @@ ALTER TABLE `tb_product`
 -- AUTO_INCREMENT for table `tb_category`
 --
 ALTER TABLE `tb_category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tb_history`
 --
 ALTER TABLE `tb_history`
-  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_history` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `tb_product`
 --
 ALTER TABLE `tb_product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `tb_role`
+--
+ALTER TABLE `tb_role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -146,7 +259,13 @@ ALTER TABLE `tb_product`
 -- Constraints for table `tb_product`
 --
 ALTER TABLE `tb_product`
-  ADD CONSTRAINT `Category` FOREIGN KEY (`category_product`) REFERENCES `tb_category` (`id_category`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `tb_product_ibfk_1` FOREIGN KEY (`category_product`) REFERENCES `tb_category` (`id_category`);
+
+--
+-- Constraints for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `Role Join` FOREIGN KEY (`access_user`) REFERENCES `tb_role` (`id_role`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- readme.md
+++ readme.md
@@ -1,12 +1,11 @@
 # POS (Backend) Application
 
 ## Modules included :
- - Body Parser
- - CORS
- - DotEnv
- - ExpressJS
- - MySQL2
- - 
+|           |        |             |        |
+| --------- | ------ | ----------- | ------ |
+| ExpressJS | MySQL2 | Moment      | Lodash |
+| JWT       | bcrypt | Body Parser | CORS   |
+| DotENV    | Multer | Redis       |
 
  ## Dev Modules :
 - ESLint
@@ -23,7 +22,7 @@
     - `DB_PASS`   : fill with PASSWORD in your database configuration (Or leave it null if your database haven't password)
     - `DB_NAME`   : fill with the NAME OF DATABASE (Or leave it filled with `db_pos` if you isn't rename the database)
     - `JWT_SECRET`   : fill with the unique value due to signature verifier on JWT
-    - `TZ` : fill in your current time zone in your area
+    - `TZ` : fill in your current time zone in your area. If you don't know, https://en.wikipedia.org/wiki/List_of_tz_database_time_zones <- you can get from this list on column `TZ database name`
 4. Run with : 
     - `npm run start-cli` if you want to run it in client mode (use node) without auto restart on every changing code
     - `npm run start-dev` if you want to run it in developer mode (use `nodemon`)  every change and save it will auto restart.
@@ -33,8 +32,11 @@
   https://docs.google.com/presentation/d/1auvDwaUJ-HIYhTpZHlU2kuStVt-r8BJCZYfJ91YRJuE/edit?usp=sharing
 
 - Postman Documentation: 
-  1. Online version : https://www.getpostman.com/collections/84af7e59cbdc58479dc1
-  2. Offline version : import from : `docs/[Postman]RZIDPOS_API.json`
+  import from : `docs/[Postman]RZIDPOS_API.postman_environment.json`. then make an environtment that save variable in Postman. 
+
+  1. `URL` : value is like `http://localhost:3000` that linked to served POS APP microservice URL
+  2. `TOKEN` : value is for put the token that you login and access CRUD of method each controller
+
 
 - Flowchart
   https://drive.google.com/file/d/18z5ICoggPqc0-xc31ayGGAo1xJa-Zzlr/view?usp=sharing
