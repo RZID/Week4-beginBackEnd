8e60bcedb1474b8cd63b58c54542ee0e8a1db961
rzid <rdhanukpb@gmail.com>
2021-02-01 10:54:22 +0700

    push revisi Coba

--- src/controller/ctrlProduct.js
+++ src/controller/ctrlProduct.js
@@ -142,12 +142,7 @@ module.exports = {
                     if (beforeImage && beforeImage !== 'default.jpg') {
                         const path = `${process.cwd()}/public/imageProduct/${beforeImage}` // * CWD is Current Working Directory (which is root folder)
                         // Process delete
-                        fs.unlink(path, (err) => {
-                            // if error, throw error
-                            if (err) {
-                                responser.internalError(res, err)
-                            }
-                        })
+                        fs.unlink(path)
                     }
                 }
             }).catch(err => responser.internalError(res, err.message))
